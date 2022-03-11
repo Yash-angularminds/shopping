10d393c55ff1473f616ed4a64d4f7ddb9dce851b
Yash-angularminds <97215057+Yash-angularminds@users.noreply.github.com>
2022-03-11 16:02:36 +0530

    updated placeOrder

--- src/Placeorder.js
+++ src/Placeorder.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 
 function Placeorder() {
 
-  let navigate = useNavigate()  
+    const [cartItems, setCartItems] = useState([])
+    const [cartCount, setCartCount] = useState()
+    const [person, setPerson] = useState('')
+    const [address, setAddress] = useState('')
+
+    useEffect(() => {
+        let temp = []
+        temp = JSON.parse(window.localStorage.getItem('items'));
+        setCartItems([...temp])
+        setCartCount(temp.length)
+    }, [])
+
+    let navigate = useNavigate()  
 
     const goToCart = () => {
         navigate('/cart');
+    }
+
+    const calculateSubTotal = (current) => {
+        // console.log(arr);
+        let arr = []
+        arr = [current]
+        let price = 0;
+        if(arr){
+            arr.map((item)=>{
+                price=item.price*item.quantity
+            })
+        }
+        return price;
+    }
+
+    const calculateAmount = (arr) => {
+        let amount=0;
+        arr.map((item)=> (
+            amount+=item.price*item.quantity
+        ))
+        return amount;
     }
     
   return (
@@ -17,7 +50,7 @@ function Placeorder() {
                 <a href="/">My Ecommerce Site</a>
                 
                 <span className="pull-right">
-                    <button onClick={()=>goToCart()}>Cart (0)</button>
+                    <button onClick={()=>goToCart()}>Cart ({cartCount})</button>
                 </span>
             </h1>
             <hr></hr>
@@ -31,27 +64,26 @@ function Placeorder() {
                                 <thead className="table-head">
                                     <tr>
                                         <td>Product Name</td>
-                                        <td> Quntity</td>
+                                        <td> Quantity</td>
                                         <td> SubTotal</td>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>H2O Sb104 Stainless Steel Sports </td>
-                                        <td>2</td>
-                                        <td><i className="fa fa-inr"></i>400</td>
+                                    {cartItems && cartItems.map((item) => (
+                                        <tr>
+                                        <td>{item.name}</td>
+                                        <td>{item.quantity}</td>
+                                        <td><i className="fa fa-inr">
+                                            {calculateSubTotal(item)}
+                                            </i></td>
                                     </tr>
-                                    <tr>
-                                        <td>bbb</td>
-                                        <td>1</td>
-                                        <td><i className="fa fa-inr"></i>1000</td>
-                                    </tr>
+                                    ))}
                                     <tr>
                                         <td><strong>Total</strong></td>
                                         <td>
-                                            <strong> 3</strong>
+                                            <strong>{cartCount}</strong>
                                         </td>
-                                        <td><strong><i className="fa fa-inr"></i>2400 </strong></td>
+                                        <td><strong><i class="fa fa-inr"></i>{calculateAmount(cartItems)}</strong></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -63,20 +95,20 @@ function Placeorder() {
                             <div className="form-group">
                                 <label for="inputName3" className="col-sm-2 control-label">Name</label>
                                 <div className="col-sm-6">
-                                    <input className="form-control" id="inputName3" placeholder="Name"></input>
+                                    <input className="form-control" id="inputName3" placeholder="Name" onChange={(e)=>setPerson(e.target.value)}></input>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label for="inputEmail3" className="col-sm-2 control-label">Address</label>
                                 <div className="col-sm-6">
                                     <textarea className="form-control" id="inputEmail3"
-                                        placeholder="Deliver Address"></textarea>
+                                        placeholder="Deliver Address" onChange={(e)=>setAddress(e.target.value)}></textarea>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-sm-2 control-label"></label>
                                 <div className="col-sm-6">
-                                    <a className="btn btn-warning">Confirm Order</a>
+                                    <button className="btn btn-warning">Confirm Order</button>
                                 </div>
                             </div>
                             </form>
